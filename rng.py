ab4195a02271a91773e47c46217b1d6069743c2e
DavidVelez <felipvelez@gmail.com>
2016-04-24 10:42:50 -0500

    rng

---
+++ rng.py
@@ -0,0 +1,25 @@
+ 
+
+#Autor: David Velez
+import numpy as np
+import matplotlib.pyplot as plt
+t= 200
+
+def rng (t):
+
+
+	x=0
+	p=0
+	for i in range (t):
+	
+	
+		
+		N=np.random.normal(0,1)
+
+	
+
+
+print 'iteracciones es:',rng(t)
+
+plt.plot(t)
+plt.show()
